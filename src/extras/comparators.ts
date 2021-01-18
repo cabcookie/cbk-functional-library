833d3079787d26d6a3df23958b08c2123642197f
Carsten Koch <carsten.b.koch@gmail.com>
2021-01-18 09:27:04 +0100

    fix: comparators were not identified as functions

--- src/extras/comparators.ts
+++ src/extras/comparators.ts
@@ -15,17 +15,15 @@ const getComparableDate = (date: Date) => (new Date(date)).getTime()
 const compareDateValues = (arr: FixedSizeArray<2, Date>) => getComparableDate(arr[0]) - getComparableDate(arr[1])
 const ascDesc = (order: AscDescString) => (val: number) => order === 'desc' ? val * -1 : val
 
-export const stringComparator = (key = '', order: AscDescString = 'asc') => flow(
-    argsToArray,
+export const stringComparator = (key = '', order: AscDescString = 'asc') => (str1: string, str2: string) => flow(
     map(get(key)),
     map(upperCase),
     compareValues,
     ascDesc(order)
-)
+)([str1, str2])
 
-export const dateComparator = (key = '', order: AscDescString = 'asc') => flow(
-    argsToArray,
+export const dateComparator = (key = '', order: AscDescString = 'asc') => (date1: Date, date2: Date) => flow(
     map(get(key)),
     compareDateValues,
     ascDesc(order),
-)
+)([date1, date2])
