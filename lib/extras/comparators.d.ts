3b6a7b7e4db902be6c063b572a3096681e8ec2a5
Carsten Koch <carsten.b.koch@gmail.com>
2021-01-18 20:53:01 +0100

    fix: deleted lib folder

--- lib/extras/comparators.d.ts
+++
@@ -1,3 +0,0 @@
-import { AscDescString, HasKey, FunctionWithTwoParamsReturnNumber } from "./type-helpers";
-export declare const stringComparator: <K extends string>(key: K, order: AscDescString) => FunctionWithTwoParamsReturnNumber<string | HasKey<K, string>>;
-export declare const dateComparator: <K extends string>(key: K, order: AscDescString) => FunctionWithTwoParamsReturnNumber<Date | HasKey<K, Date>>;
--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { checkObjectForProperty, getKey, getObjKeys, addKeyFunc, addKey, isObject, replaceNullValuesInObject } from "./objects/object-functions";
-import { mult, plus, minus, div, rest, localNumberString } from './numbers/number-functions';
-import { makeAsyncCall, tryer, addLogging, ifTypeError, defaultFn, ifThenElse, checkTypeOfValue, runIfType, runIfHasProperty, isNullOrUndefined, log, runFnOfObj } from "./higher-order/higher-order-funtions";
-import { isArray, isEmptyArray, argsToArray, createArray, isFirstItem, map, match, split, join, filter, sort, getFirstItem, filterFirstItem, filterFirstItemGetKey, slice, push, pop, shift, unshift, remove, add, move } from "./arrays/array-functions";
-import { upperCase, capitalize, replace, str, strFill, prepend, append, left, right, mid } from "./strings/string-functions";
-import { isDevEnvironment } from "./extras/other-functions";
-import { dateComparator, stringComparator } from './extras/comparators';
-import { makeDate, now, setTimer, addSeconds, addMinutes, addDays, toISOString, shortDate, shortDateTime, shortTime, secondsToHhMmSs, secondsToMmSs, minutesToHhMm } from './date-time/date-time-functions';
-export { checkObjectForProperty, getKey, getObjKeys, addKeyFunc, addKey, isObject, isDevEnvironment, dateComparator, stringComparator, replaceNullValuesInObject, mult, plus, minus, div, rest, localNumberString, makeAsyncCall, tryer, addLogging, ifTypeError, defaultFn, ifThenElse, checkTypeOfValue, runIfType, runIfHasProperty, isNullOrUndefined, log, runFnOfObj, isArray, isEmptyArray, argsToArray, createArray, isFirstItem, map, match, split, join, filter, sort, getFirstItem, filterFirstItem, filterFirstItemGetKey, slice, push, pop, shift, unshift, remove, add, move, upperCase, capitalize, replace, str, strFill, prepend, append, left, right, mid, makeDate, now, setTimer, addSeconds, addMinutes, addDays, toISOString, shortDate, shortDateTime, shortTime, secondsToHhMmSs, secondsToMmSs, minutesToHhMm, };
