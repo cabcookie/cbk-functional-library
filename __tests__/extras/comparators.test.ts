7f3fe29d4bd3d110a37a643a2c87fee01e8fd3e0
Carsten Koch <carsten.b.koch@gmail.com>
2021-01-18 20:53:37 +0100

    fix: ignore lib folder

--- __tests__/extras/comparators.test.ts
+++ __tests__/extras/comparators.test.ts
@@ -4,7 +4,16 @@ const date2 = new Date("1970-01-01")
 const dateObj1 = { date: date1 }
 const dateObj2 = { date: date2 }
 
-describe("testing data comparision with a key of an object with no order", () => {
+describe("run date comparision with wrong parameters", () => {
+    test("run without a key and with numbers", () => {
+        expect(dateComparator()(1,2)).toThrowError()
+    })
+    test("run without a key and with strings", () => {
+        expect(dateComparator()("1","2")).toThrowError()
+    })
+})
+
+describe("testing date comparision with a key of an object with no order", () => {
     test("compare same dates should return 0", () => {
         expect(dateComparator("date")(dateObj1, dateObj1)).toEqual(0)
     })
@@ -16,7 +25,7 @@ describe("testing data comparision with a key of an object with no order", () =>
     })
 })
 
-describe("testing data comparision with a key of an object with ascending order", () => {
+describe("testing date comparision with a key of an object with ascending order", () => {
     test("compare same dates should return 0", () => {
         expect(dateComparator("date", "asc")(dateObj1, dateObj1)).toEqual(0)
     })
@@ -28,7 +37,7 @@ describe("testing data comparision with a key of an object with ascending order"
     })
 })
 
-describe("testing data comparision with a key of an object with descending order", () => {
+describe("testing date comparision with a key of an object with descending order", () => {
     test("compare same dates should return 0", () => {
         expect(dateComparator("date", "desc")(dateObj1, dateObj1)).toEqual(0)
     })
@@ -40,7 +49,7 @@ describe("testing data comparision with a key of an object with descending order
     })
 })
 
-describe("testing data comparision with a date with no order", () => {
+describe("testing date comparision with a date with no order", () => {
     test("compare same dates should return 0", () => {
         expect(dateComparator()(date1, date1)).toEqual(0)
     })
@@ -52,7 +61,7 @@ describe("testing data comparision with a date with no order", () => {
     })
 })
 
-describe("testing data comparision with a date with ascending order", () => {
+describe("testing date comparision with a date with ascending order", () => {
     test("compare same dates should return 0", () => {
         expect(dateComparator("", "asc")(date1, date1)).toEqual(0)
     })
@@ -64,7 +73,7 @@ describe("testing data comparision with a date with ascending order", () => {
     })
 })
 
-describe("testing data comparision with a date with descending order", () => {
+describe("testing date comparision with a date with descending order", () => {
     test("compare same dates should return 0", () => {
         expect(dateComparator("", "desc")(date1, date1)).toEqual(0)
     })
