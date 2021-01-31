438f7a95f4b53fdee2f559c08bd35dbfc5989703
Carsten Koch <carsten.b.koch@gmail.com>
2021-01-31 18:29:37 +0100

    fix: import of defaultFn

--- src/higher-order/if-then-else.ts
+++ src/higher-order/if-then-else.ts
@@ -1,6 +1,5 @@
 import { isNotNullAndTrue } from '../boolean'
-import { defaultFn } from '../index'
+import { defaultFn } from './default-function'
 import { FunctionReturningType } from '../types'
-
 
 export const ifThenElse: <B extends object | unknown, C extends object | unknown>(ifFn: FunctionReturningType<boolean>, thenFn: FunctionReturningType<B>, elseFn?: FunctionReturningType<C>) => (...args: any) => B | C = (ifFn, thenFn, elseFn = defaultFn) => (...args) => isNotNullAndTrue(ifFn)(...args) ? thenFn(...args) : elseFn(...args)
